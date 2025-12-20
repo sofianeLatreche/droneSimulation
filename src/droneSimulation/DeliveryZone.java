f4622235bc0902da8a14fb86ae365f5500c948eb
F.Z <F.Z@sofiane>
2025-12-20 03:06:06 +0100

    DeliveryZone

---
+++ src/droneSimulation/DeliveryZone.java
@@ -0,0 +1,12 @@
+public class DeliveryZone {
+
+    protected Position center;
+    protected double radius;
+
+    public DeliveryZone(Position c , double r)
+    {
+        center = c;
+        radius = r;
+    }
+    
+}
