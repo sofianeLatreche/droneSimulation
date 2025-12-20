f632ef267a77a7a8cb62170aaee4c2a76c4e598a
F.Z <F.Z@sofiane>
2025-12-20 02:37:49 +0100

    Position

--- src/droneSimulation/Position.java
+++ src/droneSimulation/Position.java
@@ -1,5 +1,35 @@
 package droneSimulation;
 
 public class Position {
+    private double x;
+    private double y;
+
+    public Position(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Position other) {
+        return Math.hypot(this.x - other.x, this.y - other.y);
+    }
+
+    public void moveTo(Position destination, double step) {
+        double distance = distanceTo(destination);
+        if (distance <= step) {
+            this.x = destination.x;
+            this.y = destination.y;
+            return;
+        }
+        this.x += (destination.x - x) / distance * step;
+        this.y += (destination.y - y) / distance * step;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+
 
 }
+
