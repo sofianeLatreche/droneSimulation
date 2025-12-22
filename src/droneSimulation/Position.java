36365686100ae9b40f159300035006625ee95779
Saadihalla <saadihalla10@gmail.com>
2025-12-22 16:24:37 +0100

    Update Position.java

--- src/droneSimulation/Position.java
+++ src/droneSimulation/Position.java
@@ -1,51 +1,53 @@
 package droneSimulation;
 
 public class Position {
-    private double x;
-    private double y;
-
+    private double x; 
+    private double y; 
+    
     public Position(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
     public double distanceTo(Position other) {
-        return Math.hypot(this.x - other.x, this.y - other.y);
+        if (other == null) return 0;
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
     }
-
+    
+    // Move towards destination by a certain step
     public void moveTo(Position destination, double step) {
+        if (destination == null || step <= 0) return;
+        
         double distance = distanceTo(destination);
         if (distance <= step) {
             this.x = destination.x;
             this.y = destination.y;
-            return;
+        } else {
+            double ratio = step / distance;
+            this.x = this.x + (destination.x - this.x) * ratio;
+            this.y = this.y + (destination.y - this.y) * ratio;
         }
-        this.x += (destination.x - x) / distance * step;
-        this.y += (destination.y - y) / distance * step;
     }
-
     public double getX() { return x; }
     public double getY() { return y; }
-
     public void setX(double x) { this.x = x; }
     public void setY(double y) { this.y = y; }
-
+    
     @Override
-    public boolean equals(Object o)
-    {
-        Position p = (Position) o;
-
-        if (x == p.x && y == p.y)
-            return true;
-
-        return false;
+    public String toString() {
+        return String.format("Position(%.2f, %.2f)", x, y);
     }
-
+    
     @Override
-    public String toString()
-    {
-        return "x = " + x + " , y = " + y;
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Position position = (Position) obj;
+        return Math.abs(position.x - x) < 0.001 && Math.abs(position.y - y) < 0.001; //beacuse floating numbers are not precise 
+    }
+    
+    @Override
+    public int hashCode() {
+        return Double.hashCode(x) * 31 + Double.hashCode(y);//creates a unique code for each objects so it is easy to find and use 
     }
 }
-
-
