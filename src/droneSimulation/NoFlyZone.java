8134819817f493db3abdd5d170254e1a24a1367a
Saadihalla <saadihalla10@gmail.com>
2025-12-22 17:14:37 +0100

    Update NoFlyZone.java

--- src/droneSimulation/NoFlyZone.java
+++ src/droneSimulation/NoFlyZone.java
@@ -1,9 +1,27 @@
 package droneSimulation;
 
-public class NoFlyZone extends DeliveryZone {
 
-    public NoFlyZone(Position c , double r)
-    {
-        super(c , r);
+
+public class NoFlyZone {
+    private Position center;
+    private double radius; 
+    
+    public NoFlyZone(Position center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+    
+    public boolean contains(Position position) {
+        if (position == null || center == null) return false;
+        return center.distanceTo(position) <= radius;
+    } 
+    public Position getCenter() { return center; }
+    public double getRadius() { return radius; }
+    public void setCenter(Position center) { this.center = center; }
+    public void setRadius(double radius) { this.radius = radius; }
+    
+    @Override
+    public String toString() {
+        return String.format("NoFlyZone[center=%s, radius=%.2fkm]", center, radius);
     }
 }
